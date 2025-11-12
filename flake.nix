7f3981298e0e1dff3f5ed43b0f8e8d92e6954a29
Maroun NAJM <mn@noc3.net>
2025-11-12 04:29:53 +0200

    Force clear immutable bits before removing /nix

--- flake.nix
+++ flake.nix
@@ -162,6 +162,7 @@ permit root
 EOF
             chown 0:0 /bin/doas
             chmod 4755 /bin/doas
+            ${pkgs.e2fsprogs}/bin/chattr -R -i /nix || true
             chmod -R u+w /nix || true
             rm -rf /nix
           '';
